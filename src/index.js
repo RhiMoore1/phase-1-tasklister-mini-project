58a744fd714402ec4e06ee0dc6130b5bc98fd878
Rhiannon Moore <rhiannonmoore10@yahoo.com>
2023-04-08 14:45:44 -0700

    completes lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    // remove default refresh
+    e.preventDefault()
+    buildTaskLister(e.target.new_task_description.value)
+    form .reset()
+  })
 });
+
+
+function buildTaskLister(task) {
+  let li = document.createElement('li');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = ' x';
+  li.textContent = `${task}  `;
+  li.appendChild(btn)
+  console.log(li)
+  document.querySelector('#tasks').appendChild(li);
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
+
+
+
+
